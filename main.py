4b444cd4981ff221076c632735c8701c93cdf2f8
mattleinwand <mattleinwand@berkeley.edu>
2022-12-09 16:18:44 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[ ]:
-
-
 if __name__ == "__main__":
     def some_function():
         print("Hello World!")
 
-some_function()
+    some_function()
 
